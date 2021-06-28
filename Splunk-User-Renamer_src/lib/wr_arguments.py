8a083bf8c5d7c0854bda70bc6b0a83ebc7b0fa6d
will <RootP@ss?1>
2021-06-27 18:30:39 -0700

    sh file, normalize args

--- Splunk-User-Renamer_src/lib/wr_arguments.py
+++ Splunk-User-Renamer_src/lib/wr_arguments.py
@@ -24,7 +24,7 @@ class LoadFromFile (argparse.Action):
 ### FUNCTIONS ###########################################
 
 def str2bool(string: str) -> bool:
-	#Define common "string values" for bool args to accept "Yes" as well as "True"
+	#Define common 'string values' for bool args to accept 'Yes' as well as 'True'
 	if isinstance(string, bool):
 		return(string)
 	if string.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -37,35 +37,35 @@ def str2bool(string: str) -> bool:
 def checkPositive(value: str) -> int:
 	ivalue = int(value)
 	if ivalue < 0:
-		raise argparse.ArgumentTypeError("%s is an invalid (below 0) int value" % value)
+		raise argparse.ArgumentTypeError('%s is an invalid (below 0) int value' % value)
 	return ivalue
 
 def checkCSVHeader(string:str):
 	if args.csv_file:
-		if string == "None":
+		if string == 'None':
 			raise argparse.ArgumentTypeError('CSV Specified, -csv_f / --csv_header needs be specified as well, True or False.')
 		else:
 			return(str2bool(string))
 
 def Arguments():
 	# Arguments the app will accept
-	parser.add_argument('--file', type=open, action=LoadFromFile)
-	parser.add_argument("-buf", "--backup_folder", nargs='?', default='./backup/', required=False, help="Relative to this script or full path to where backup folders and files will be copied to.")
-	parser.add_argument("-csv", "--csv_folder", nargs='?', default='', required=False, help="Relative to this script or full path to csv file folder (not the file name itself) to read change map. Assumes column 0 is old names and 1 is new names.")
-	parser.add_argument("-csv_h", "--csv_header", type=checkCSVHeader, nargs='?', const=True, default="None", required=False, help="True or False whether CSV contains header fields or not.")
-	parser.add_argument("-csv_old_col", "--csv_old_uname_col", type=checkPositive, nargs='?', default=0, required=False, help="Column number in CSV (left to right) old usernames are in. Default is 0 for FIRST column")
-	parser.add_argument("-csv_new_col", "--csv_new_uname_col", type=checkPositive, nargs='?', default=1, required=False, help="Column number in CSV (left to right) new usernames are in. Default is 1 for SECOND column")
-	parser.add_argument("-up", "--uname_prefeix", nargs='?', default='', required=False, help="If CSV is not being used, you may mass rename users to have this prefix. If CSV is specified, this is ignored.")
-	parser.add_argument("-us", "--uname_suffix", nargs='?', default='', required=False, help="If CSV is not being used, you may mass rename users to have this suffix. If CSV is specified, this is ignored.")
-	parser.add_argument("-sph", "--splunk_home", nargs='?', defaultopt='/opt/splunk/', required=False, help="Full path to Splunk's install dir.")
-	parser.add_argument("-ll", "--log_level", type=checkPositive, nargs='?', default=1, required=False, help="1-3, 1 being less, 3 being most")
-	parser.add_argument('-fsl', '--file_search_list', nargs='*', default=[], required=False, help="List of values the filename must have in order to search in, separated by commas, i.e: 'server.conf,prop' ")
-	parser.add_argument("-fslt", "--file_search_list_type", type=str2bool, nargs='?', const=True, default=False,  required=False, help="True for each item in file_search_list to have to be an exact match, False for contains. If using contains, search in can be lessened to wild cards like 'container' means '*container*'. Do NOT use *, they are implied. ")
-	parser.add_argument('-figl', '--file_ignore_list', nargs='*', default=[], required=False, help="List of values the filename has to ignore AFTER search in list has finished, separated by commas, i.e: 'server1.conf,props_' ")
-	parser.add_argument("-figlt", "--file_ignore_list_type", type=str2bool, nargs='?', const=True, default=False,  required=False, help="True for each item in file_ignore_list to have to be an exact match, False for contains. If using contains, search in can be lessened to wild cards like 'frozenda' means '*frozenda*'. Do NOT use *, they are implied. ")
-	parser.add_argument('-fn', '--file_names', nargs='*', default=['local.meta'], required=False, help="List of file names to search in, separated by commas. i.e: 'local.meta'. User directories are handled by default, this is for file contents. ")
-	parser.add_argument("-dm", "--debug_modules", type=str2bool, nargs='?', const=True, default=False,  required=False, help="Will enable deep level debug on all the modules that make up the script. Enable if getting errors, to help dev pinpoint.")
-	parser.add_argument("-tr", "--test_run", type=str2bool, nargs='?', const=True, default=False,  required=False, help="If True, nothing will be modified, only reports what WOULD be modified. Logging is normal.")
+	parser.add_argument("--file", type=open, action=LoadFromFile)
+	parser.add_argument("-buf", "--backup_folder", nargs="?", default="./backup/", required=False, help="Relative to this script or full path to where backup folders and files will be copied to.")
+	parser.add_argument("-csv", "--csv_folder", nargs="?", default="", required=False, help="Relative to this script or full path to csv file folder (not the file name itself) to read change map. Assumes column 0 is old names and 1 is new names.")
+	parser.add_argument("-csv_h", "--csv_header", type=checkCSVHeader, nargs="?", const=True, default="None", required=False, help="True or False whether CSV contains header fields or not.")
+	parser.add_argument("-csv_old_col", "--csv_old_uname_col", type=checkPositive, nargs="?", default=0, required=False, help="Column number in CSV (left to right) old usernames are in. Default is 0 for FIRST column")
+	parser.add_argument("-csv_new_col", "--csv_new_uname_col", type=checkPositive, nargs="?", default=1, required=False, help="Column number in CSV (left to right) new usernames are in. Default is 1 for SECOND column")
+	parser.add_argument("-up", "--uname_prefeix", nargs="?", default="", required=False, help="If CSV is not being used, you may mass rename users to have this prefix. If CSV is specified, this is ignored.")
+	parser.add_argument("-us", "--uname_suffix", nargs="?", default="", required=False, help="If CSV is not being used, you may mass rename users to have this suffix. If CSV is specified, this is ignored.")
+	parser.add_argument("-sph", "--splunk_home", nargs="?", default="/opt/splunk/", required=False, help="Full path to Splunk's install dir.")
+	parser.add_argument("-ll", "--log_level", type=checkPositive, nargs="?", default=1, required=False, help="1-3, 1 being less, 3 being most")
+	parser.add_argument("-fsl", "--file_search_list", nargs="*", default=[], required=False, help="List of values the filename must have in order to search in, separated by commas, i.e: 'server.conf,prop' ")
+	parser.add_argument("-fslt", "--file_search_list_type", type=str2bool, nargs="?", const=True, default=False,  required=False, help="True for each item in file_search_list to have to be an exact match, False for contains. If using contains, search in can be lessened to wild cards like 'container' means '*container*'. Do NOT use *, they are implied. ")
+	parser.add_argument("-figl", "--file_ignore_list", nargs="*", default=[], required=False, help="List of values the filename has to ignore AFTER search in list has finished, separated by commas, i.e: 'server1.conf,props_' ")
+	parser.add_argument("-figlt", "--file_ignore_list_type", type=str2bool, nargs="?", const=True, default=False,  required=False, help="True for each item in file_ignore_list to have to be an exact match, False for contains. If using contains, search in can be lessened to wild cards like 'frozenda' means '*frozenda*'. Do NOT use *, they are implied. ")
+	parser.add_argument("-fn", "--file_names", nargs="*", default=['local.meta'], required=False, help="List of file names to search in, separated by commas. i.e: 'local.meta'. User directories are handled by default, this is for file contents. ")
+	parser.add_argument("-dm", "--debug_modules", type=str2bool, nargs="?", const=True, default=False,  required=False, help="Will enable deep level debug on all the modules that make up the script. Enable if getting errors, to help dev pinpoint.")
+	parser.add_argument("-tr", "--test_run", type=str2bool, nargs="?", const=True, default=False,  required=False, help="If True, nothing will be modified, only reports what WOULD be modified. Logging is normal.")
 
 ############## RUNTIME
 Arguments()
---
+++ Splunk-User-Renamer_src/spur.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+### Python3
+python3 spur.py \
+    -buf './backups' \
+    -csv './csvs/' \
+    -csv_h True \
+    -csv_old_col 0 \
+    -csv_new_col 1 \
+    -sph '/opt/splunk/' \
+    -ll 3 \
+    -fn 'local.meta' \
+    -dm True \
+    -tr True
+
+# SEE README FOR ARG DETAILS
+# You should always try and use this version as it doesn't need any outside dependencies
+# \  = indicates cmd continues on next line in bash
+# -buf = --backup_folder | Relative to this script or full path to where backup folders and files will be copied to.
+# -csv = --csv_folder | Relative to this script or full path to csv file folder (not the file name itself) to read change map. Assumes column 0 is old names and 1 is new names.
+# -csv_h = --csv_header | True or False whether CSV contains header fields or not.
+# -csv_old_col = --csv_old_uname_col | Column number in CSV (left to right) old usernames are in. Default is 0 for FIRST column
+# -csv_new_col = --csv_new_uname_col | Column number in CSV (left to right) new usernames are in. Default is 1 for SECOND column
+# -up = --uname_prefeix | If CSV is not being used, you may mass rename users to have this prefix. If CSV is specified, this is ignored.
+# -us = --uname_suffix | If CSV is not being used, you may mass rename users to have this suffix. If CSV is specified, this is ignored.
+# -sph = --splunk_home | Full path to Splunk's install dir.
+# -ll = --log_level | 1-3, 1 being less, 3 being most
+# -fsl = --file_search_list | List of values the filename must have in order to search in, separated by commas, i.e: 'server.conf,prop' 
+# -fslt = --file_search_list_type | True for each item in file_search_list to have to be an exact match, False for contains. If using contains, search in can be lessened to wild cards like 'container' means '*container*'. Do NOT use *, they are implied. 
+# -figl = --file_ignore_list | List of values the filename has to ignore AFTER search in list has finished, separated by commas, i.e: 'server1.conf,props_' 
+# -figlt = --file_ignore_list_type | True for each item in file_ignore_list to have to be an exact match, False for contains. If using contains, search in can be lessened to wild cards like 'frozenda' means '*frozenda*'. Do NOT use *, they are implied. 
+# -fn = --file_name | List of file names to search in, separated by commas. i.e: 'local.meta'. User directories are handled by default, this is for file contents. 
+# -dm = --debug_modules | Will enable deep level debug on all the modules that make up the script. Enable if getting errors, to help dev pinpoint.
+# -tr = --test_run | If True, nothing will be modified, only reports what WOULD be modified. Logging is normal.
